1c1e6b5abce6a76d38170dd423ed19259b00c021
AlanElliott82 <64148143+AlanElliott82@users.noreply.github.com>
2025-03-11 14:53:41 +0000

    Add files via upload

---
+++ NewIPNIRecords_top.py
@@ -0,0 +1,338 @@
+## Script to split monthly IPNI records once matching into csv's for distribution to TENs. 
+## Take the IPNI export add to WFOsqlite.db as IPNI{month}. Search and replace current three letter month for current.
+## Script needs updated when new TEN's added. 
+## Edits to make - Some multifamily TENs need combined. Commelinales check others.
+
+import sqlite3
+import csv
+import datetime
+
+# Get the current time
+time = datetime.datetime.now
+
+# Connect to the SQLite database (or create it if it doesn't exist)
+conn = sqlite3.connect('data/WFOsqlite.db')
+
+# Create a cursor object to interact with the database
+cursor = conn.cursor()
+
+# Caryophyllales file
+# Execute the SQL query
+cursor.execute('''SELECT IPNIJan.rhakhis_wfo AS WFOID,
+       IPNIJan.id AS IPNID,
+       IPNIJan.taxon_scientific_name_s_lower AS scientificName,
+       IPNIJan.authors_t,
+       IPNIJan.reference_t AS namePublishedin,
+       IPNIJan.family_s_lower AS Family,
+       IPNIJan.name_status_s_lower AS nomenclaturalStatus,
+       IPNIJan.basionym_s_lower AS originalName,
+       IPNIJan.basionym_author_s_lower AS originalNameAuthor
+  FROM IPNIJan
+       INNER JOIN
+       CarophyllalesFamilies
+ WHERE CarophyllalesFamilies.Family = IPNIJan.family_s_lower;
+  ''')
+
+# Fetch all the results
+results = cursor.fetchall()
+
+if results:
+        # Get the column names
+        column_names = [description[0] for description in cursor.description]
+
+        # Write the results to a CSV file
+        with open('newIPNIrecords/Jan25/Caryophyllales_Jan.csv', 'w', newline='', encoding='utf-8') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            # Write the column headers
+            csv_writer.writerow(column_names)
+            # Write the data rows
+            csv_writer.writerows(results)
+
+        print(f"Records returned from Caryophyllales query.")
+        # Log the information to a file
+        with open('newIPNIrecords/Jan25/Result_log.txt', 'a') as log_file:
+            log_file.write(f'Records returned from the Caryophyllales query executed at: ' + str(time()) + '\n')
+
+else:
+        # No records found, log or record this information
+        print(f"No records returned from Caryophyllales query.")
+        # Log the information to a file
+        with open('newIPNIrecords/Jan25/noResult_log.txt', 'a') as log_file:
+            log_file.write(f'No records returned from the Caryophyllales query executed at: ' + str(time()) + '\n')
+
+## PPG Families
+
+# Execute the SQL query
+cursor.execute('''SELECT IPNIJan.rhakhis_wfo AS WFOID,
+       IPNIJan.id AS IPNID,
+       IPNIJan.taxon_scientific_name_s_lower AS scientificName,
+       IPNIJan.authors_t,
+       IPNIJan.reference_t AS namePublishedin,
+       IPNIJan.family_s_lower AS Family,
+       IPNIJan.name_status_s_lower AS nomenclaturalStatus,
+       IPNIJan.basionym_s_lower AS originalName,
+       IPNIJan.basionym_author_s_lower AS originalNameAuthor
+  FROM IPNIJan
+       INNER JOIN
+       PPGFamilies
+ WHERE PPGFamilies.Family = IPNIJan.family_s_lower;
+  ''')
+
+# Fetch all the results
+results = cursor.fetchall()
+
+if results:
+        # Get the column names
+        column_names = [description[0] for description in cursor.description]
+
+        # Write the results to a CSV file
+        with open('newIPNIrecords/Jan25/PPGFamilies_Jan.csv', 'w', newline='', encoding='utf-8') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            # Write the column headers
+            csv_writer.writerow(column_names)
+            # Write the data rows
+            csv_writer.writerows(results)
+
+        print(f"Records returned from PPGFamilies query.")
+        # Log the information to a file
+        with open('newIPNIrecords/Jan25/Result_log.txt', 'a') as log_file:
+            log_file.write(f'Records returned from the PPGFamilies query executed at: ' + str(time()) + '\n')
+
+else:
+        # No records found, log or record this information
+        print(f"No records returned from PPGFamilies query.")
+        # Log the information to a file
+        with open('newIPNIrecords/Jan25/noResult_log.txt', 'a') as log_file:
+            log_file.write(f'No records returned from the PPGFamilies query executed at: ' + str(time()) + '\n')
+
+## Conifers Families
+
+# Execute the SQL query
+cursor.execute('''SELECT IPNIJan.rhakhis_wfo AS WFOID,
+       IPNIJan.id AS IPNID,
+       IPNIJan.taxon_scientific_name_s_lower AS scientificName,
+       IPNIJan.authors_t,
+       IPNIJan.reference_t AS namePublishedin,
+       IPNIJan.family_s_lower AS Family,
+       IPNIJan.name_status_s_lower AS nomenclaturalStatus,
+       IPNIJan.basionym_s_lower AS originalName,
+       IPNIJan.basionym_author_s_lower AS originalNameAuthor
+  FROM IPNIJan
+       INNER JOIN
+       Conifers
+ WHERE Conifers.Family = IPNIJan.family_s_lower;
+  ''')
+
+# Fetch all the results
+results = cursor.fetchall()
+
+if results:
+        # Get the column names
+        column_names = [description[0] for description in cursor.description]
+
+        # Write the results to a CSV file
+        with open('newIPNIrecords/Jan25/Conifers_Jan.csv', 'w', newline='', encoding='utf-8') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            # Write the column headers
+            csv_writer.writerow(column_names)
+            # Write the data rows
+            csv_writer.writerows(results)
+
+        print(f"Records returned from Conifers families query.")
+        # Log the information to a file
+        with open('newIPNIrecords/Jan25/Result_log.txt', 'a') as log_file:
+            log_file.write(f'Records returned from the Conifers Families query executed at: ' + str(time()) + '\n')
+
+else:
+        # No records found, log or record this information
+        print(f"No records returned from Conifers Families query.")
+        # Log the information to a file
+        with open('newIPNIrecords/Jan25/noResult_log.txt', 'a') as log_file:
+            log_file.write(f'No records returned from the Conifers families query executed at: ' + str(time()) + '\n')
+
+## Cycads Families
+
+# Execute the SQL query
+cursor.execute('''SELECT IPNIJan.rhakhis_wfo AS WFOID,
+       IPNIJan.id AS IPNID,
+       IPNIJan.taxon_scientific_name_s_lower AS scientificName,
+       IPNIJan.authors_t,
+       IPNIJan.reference_t AS namePublishedin,
+       IPNIJan.family_s_lower AS Family,
+       IPNIJan.name_status_s_lower AS nomenclaturalStatus,
+       IPNIJan.basionym_s_lower AS originalName,
+       IPNIJan.basionym_author_s_lower AS originalNameAuthor
+  FROM IPNIJan
+       INNER JOIN
+       Cycads
+ WHERE Cycads.Family = IPNIJan.family_s_lower;
+  ''')
+
+# Fetch all the results
+results = cursor.fetchall()
+
+if results:
+        # Get the column names
+        column_names = [description[0] for description in cursor.description]
+
+        # Write the results to a CSV file
+        with open('newIPNIrecords/Jan25/Cycads_Jan.csv', 'w', newline='', encoding='utf-8') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            # Write the column headers
+            csv_writer.writerow(column_names)
+            # Write the data rows
+            csv_writer.writerows(results)
+
+        print(f"Records returned from Cycads query.")
+        # Log the information to a file
+        with open('newIPNIrecords/Jan25/Result_log.txt', 'a') as log_file:
+            log_file.write(f'Records returned from the Cycads query executed at: ' + str(time()) + '\n')
+
+else:
+        # No records found, log or record this information
+        print(f"No records returned from Cycads query.")
+        # Log the information to a file
+        with open('newIPNIrecords/Jan25/noResult_log.txt', 'a') as log_file:
+            log_file.write(f'No records returned from the Cycads query executed at: ' + str(time()) + '\n')
+
+## Haloragaceae Families
+
+# Execute the SQL query
+cursor.execute('''SELECT IPNIJan.rhakhis_wfo AS WFOID,
+       IPNIJan.id AS IPNID,
+       IPNIJan.taxon_scientific_name_s_lower AS scientificName,
+       IPNIJan.authors_t,
+       IPNIJan.reference_t AS namePublishedin,
+       IPNIJan.family_s_lower AS Family,
+       IPNIJan.name_status_s_lower AS nomenclaturalStatus,
+       IPNIJan.basionym_s_lower AS originalName,
+       IPNIJan.basionym_author_s_lower AS originalNameAuthor
+  FROM IPNIJan
+       INNER JOIN
+       Haloragaceae
+ WHERE Haloragaceae.Family = IPNIJan.family_s_lower;
+  ''')
+
+# Fetch all the results
+results = cursor.fetchall()
+
+if results:
+        # Get the column names
+        column_names = [description[0] for description in cursor.description]
+
+        # Write the results to a CSV file
+        with open('newIPNIrecords/Jan25/Haloragaceae_Jan.csv', 'w', newline='', encoding='utf-8') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            # Write the column headers
+            csv_writer.writerow(column_names)
+            # Write the data rows
+            csv_writer.writerows(results)
+
+        print(f"Records returned from Haloragaceae and sister families query.")
+        # Log the information to a file
+        with open('newIPNIrecords/Jan25/Result_log.txt', 'a') as log_file:
+            log_file.write(f'Records returned from the Haloragaceae and sister Families query executed at: ' + str(time()) + '\n')
+
+else:
+        # No records found, log or record this information
+        print(f"No records returned from Haloragaceae and sister Families query.")
+        # Log the information to a file
+        with open('newIPNIrecords/Jan25/noResult_log.txt', 'a') as log_file:
+            log_file.write(f'No records returned from the Haloragaceae and sister families query executed at: ' + str(time()) + '\n')
+
+## Legumes - Fabaceae, Leguminosae, Caesalpiniaceae names used in IPNI
+
+# Execute the SQL query
+cursor.execute('''SELECT IPNIJan.rhakhis_wfo AS WFOID,
+       IPNIJan.id AS IPNID,
+       IPNIJan.taxon_scientific_name_s_lower AS scientificName,
+       IPNIJan.authors_t,
+       IPNIJan.reference_t AS namePublishedin,
+       IPNIJan.family_s_lower AS Family,
+       IPNIJan.name_status_s_lower AS nomenclaturalStatus,
+       IPNIJan.basionym_s_lower AS originalName,
+       IPNIJan.basionym_author_s_lower AS originalNameAuthor
+  FROM IPNIJan
+       INNER JOIN
+       Legumes
+ WHERE Legumes.Family = IPNIJan.family_s_lower;
+  ''')
+
+# Fetch all the results
+results = cursor.fetchall()
+
+if results:
+        # Get the column names
+        column_names = [description[0] for description in cursor.description]
+
+        # Write the results to a CSV file
+        with open('newIPNIrecords/Jan25/Legumes_Jan.csv', 'w', newline='', encoding='utf-8') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            # Write the column headers
+            csv_writer.writerow(column_names)
+            # Write the data rows
+            csv_writer.writerows(results)
+
+        print(f"Records returned from Legume families query.")
+        # Log the information to a file
+        with open('newIPNIrecords/Jan25/Result_log.txt', 'a') as log_file:
+            log_file.write(f'Records returned from the Legume Families query executed at: ' + str(time()) + '\n')
+
+else:
+        # No records found, log or record this information
+        print(f"No records returned from Legumes Families query.")
+        # Log the information to a file
+        with open('newIPNIrecords/Jan25/noResult_log.txt', 'a') as log_file:
+            log_file.write(f'No records returned from the Legume families query executed at: ' + str(time()) + '\n')
+
+## Ten Families working on one group
+
+# Step 1: Retrieve the family list from the PPG Family List
+cursor.execute('''SELECT Family FROM TENFams''')
+query_list = [row[0] for row in cursor.fetchall()]
+
+# Step 2: Loop through the family and execute an SQL query for each value
+for family_entry in query_list:
+    # Execute the SQL query
+    cursor.execute('''SELECT IPNIJan.rhakhis_wfo AS WFOID,
+        IPNIJan.id AS IPNID,
+        IPNIJan.taxon_scientific_name_s_lower AS scientificName,
+        IPNIJan.authors_t,
+        IPNIJan.reference_t AS namePublishedin,
+        IPNIJan.name_status_s_lower AS nomenclaturalStatus,
+        IPNIJan.basionym_s_lower AS originalName,
+        IPNIJan.basionym_author_s_lower AS originalNameAuthor
+    FROM IPNIJan WHERE IPNIJan.family_s_lower = ?''', (family_entry,))
+
+    # Fetch all the results
+    results = cursor.fetchall()
+    
+    if results:
+        # Get the column names
+        column_names = [description[0] for description in cursor.description]
+
+        # Create the file name based on the entry
+        file_name = f'newIPNIrecords/Jan25/{family_entry}_Jan.csv'
+        print(file_name)
+
+        # Write the results to a CSV file
+        with open(file_name, 'w', newline='', encoding='utf-8') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            # Write the column headers
+            csv_writer.writerow(column_names)
+            # Write the data rows
+            csv_writer.writerows(results)
+        #not records found
+        print(f"Records returned from {family_entry} query.")
+        # Log the information to a file
+        with open('newIPNIrecords/Jan25/Result_log.txt', 'a') as log_file:
+            log_file.write(f'Records returned from the {family_entry} query executed at: ' + str(time()) + '\n')
+
+    else:
+        # No records found, log or record this information
+        print(f"No records returned from {family_entry} query.")
+        # Log the information to a file
+        with open('newIPNIrecords/Jan25/noResult_log.txt', 'a') as log_file:
+            log_file.write(f'No records returned from the {family_entry} query executed at: ' + str(time()) + '\n')
+# Close the connection
+conn.close()
---
+++ data/PPGFams.sql
@@ -0,0 +1,13 @@
+-- SQLite
+SELECT IPNIJan.rhakhis_wfo AS WFOID,
+       IPNIJan.id AS IPNID,
+       IPNIJan.taxon_scientific_name_s_lower AS scientificName,
+       IPNIJan.authors_t,
+       IPNIJan.reference_t AS namePublishedin,
+       IPNIJan.name_status_s_lower AS nomenclaturalStatus,
+       IPNIJan.basionym_s_lower AS originalName,
+       IPNIJan.basionym_author_s_lower AS originalNameAuthor
+  FROM IPNIJan
+       INNER JOIN
+       PPGFamilies
+ WHERE PPGFamilies.Family = IPNIJan.family_s_lower;
